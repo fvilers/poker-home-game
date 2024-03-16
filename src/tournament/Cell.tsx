1c72bde6e35fdfd1316a3fa5e2233e732843879b
Fabian Vilers <fabian.vilers@dev-one.com>
2024-03-16 08:12:41 +0100

    Update cell font size and header color

--- src/tournament/Cell.tsx
+++ src/tournament/Cell.tsx
@@ -10,11 +10,11 @@ function Cell({ children, className, title }: Props) {
   return (
     <div
       className={twMerge(
-        "flex flex-col bg-gray-50 p-4 ring-1 ring-gray-300",
+        "flex flex-col bg-gray-50 p-4 text-xl ring-1 ring-gray-300",
         className,
       )}
     >
-      <h3 className="text-base font-semibold leading-6 text-gray-900">
+      <h3 className="mb-1 text-base font-semibold leading-6 text-gray-500">
         {title}
       </h3>
 
